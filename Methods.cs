c958f457e0823c1a02ab4c592edce3858a4ccebf
aont <220471+aont@users.noreply.github.com>
2010-11-06 22:47:49 +0900

    003

---
+++ Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SubstrateSharp
+{
+    class Methods
+    {
+        static public Random rnd = new Random();
+        static public int random(int i)
+        {
+            return rnd.Next(i);
+        }
+        
+        static public int random(int a, int b)
+        {
+            return rnd.Next(a,a);
+        }
+        
+        static public double random(double a, double b)
+        {
+            return a + (b - a) * rnd.NextDouble(); 
+        }
+
+    }
+}
---
+++ Substrate.designer.cs
@@ -0,0 +1,97 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System;
+
+namespace SubstrateSharp
+{
+
+    partial class Substrate 
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Substrate
+            // 
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Substrate";
+            this.Text = "Substrate";
+            this.Resize += new System.EventHandler(this.Substrate_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private void myInitializeComponent()
+        {
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Size = new System.Drawing.Size(dimx, dimy);
+            // 
+            // Substrate
+            // 
+            this.ClientSize = new System.Drawing.Size(dimx,dimy);
+        }
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            draw();
+        }
+        void background()
+        {
+            Graphics g = Graphics.FromImage(bmd);
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, dimx, dimy);
+            g.Dispose();
+        }
+        //private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
